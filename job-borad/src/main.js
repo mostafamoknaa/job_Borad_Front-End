79436b7a239b908ed5892613cff5f8ad821e4cd2
Mostafa <mostafamokna78@gmail.com>
2025-05-02 15:35:27 +0300

    single job

--- job-borad/src/main.js
+++ job-borad/src/main.js
@@ -1,5 +1,12 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+import router from './router'
+import '@fortawesome/fontawesome-free/css/all.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
-createApp(App).mount('#app')
+
+
+createApp(App)
+    .use(router)
+    .mount('#app')
---
+++ job-borad/src/pages/HomePage.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+      <h1>Home Page</h1>
+      <router-link to="/single">Go to Single Page</router-link>
+    </div>
+  </template>
+  
