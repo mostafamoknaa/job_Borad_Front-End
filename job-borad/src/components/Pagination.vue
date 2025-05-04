6919a3098ef5359cf947d487100327bc0dc29aac
saramahmoud7689 <saramahmoud7689@gmail.com>
2025-05-05 01:43:28 +0300

    jobs

---
+++ job-borad/src/components/Pagination.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="flex justify-center mt-4">
+    <button class="px-4 py-2 border rounded hover:bg-gray-100 mx-1">Previous</button>
+    <button class="px-4 py-2 border rounded hover:bg-gray-100 mx-1">1</button>
+    <button class="px-4 py-2 border rounded hover:bg-gray-100 mx-1">2</button>
+    <button class="px-4 py-2 border rounded hover:bg-gray-100 mx-1">Next</button>
+  </div>
+</template>
